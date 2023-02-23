9727873d31f7b84d5235b475b16d09b8f61f65da
Akhil Shah <shah.akhilp@icloud.com>
2023-02-23 22:31:51 +0000

    Cast argments of function given to quickcheck, so it runs when we have a typeclass type as an argument

--- 99hs/test/Spec.hs
+++ 99hs/test/Spec.hs
@@ -5,43 +5,14 @@ import Q2 (prop_myButLast)
 
 main :: IO ()
 main = do
-    -- These tests currently work when loading q1.hs or q2.hs into ghci and running
-    -- `quickCheck` from there.
+    -- When using quickCheck, we must cast the arguments of a function, if any of
+    -- them are typeclass arguments
+    -- (see https://stackoverflow.com/questions/10037923/haskell-quickcheck-best-practices-especially-when-testing-type-classes)
 
-    -- However, these tests don't work when running `stack test`, with the following error:
+    quickCheck (prop_myLast :: [Int] -> Property)
+    quickCheck (prop_myLast :: String -> Property)
 
-    --     /Users/akhilshah/Documents/haskell/99-haskell-problems/99hs/test/Spec.hs:9:5: error:
-    --     • Ambiguous type variable ‘a1’ arising from a use of ‘quickCheck’
-    --       prevents the constraint ‘(Arbitrary a1)’ from being solved.
-    --       Probable fix: use a type annotation to specify what ‘a1’ should be.
-    --       These potential instances exist:
-    --         instance (Arbitrary a, Arbitrary b) => Arbitrary (Either a b)
-    --           -- Defined in ‘Test.QuickCheck.Arbitrary’
-    --         instance Arbitrary Ordering
-    --           -- Defined in ‘Test.QuickCheck.Arbitrary’
-    --         instance Arbitrary a => Arbitrary (Maybe a)
-    --           -- Defined in ‘Test.QuickCheck.Arbitrary’
-    --         ...plus 19 others
-    --         ...plus 62 instances involving out-of-scope types
-    --         (use -fprint-potential-instances to see them all)
-    --     • In a stmt of a 'do' block: quickCheck prop_myButLast
-    --       In the expression:
-    --         do quickCheck prop_myLast
-    --            quickCheck prop_myButLast
-    --            putStrLn "tests done!"
-    --       In an equation for ‘main’:
-    --           main
-    --             = do quickCheck prop_myLast
-    --                  quickCheck prop_myButLast
-    --                  putStrLn "tests done!"
-    --   |
-    -- 9 |     quickCheck prop_myButLast
+    quickCheck (prop_myButLast :: [Int] -> Property)
+    quickCheck (prop_myButLast :: String -> Property)
 
-
-    -- They do however work when we remove the `Eq a` constraint and change `a` to
-    -- a type that already satisfies this constraint, e.g. and Int. I'm not sure why
-    -- atm.
-
-    quickCheck prop_myLast
-    quickCheck prop_myButLast
     putStrLn "tests done!"
