a1cf18e9c6999ee9ff69467c3cf6e4acfd7930af
Akhil Shah <shah.akhilp@icloud.com>
2023-02-24 15:41:48 +0000

    Prettify formatting

--- 99hs/src/q24.hs
+++ 99hs/src/q24.hs
@@ -4,5 +4,5 @@ import System.Random
 
 diffSelect :: Int -> Int -> [Int]
 diffSelect n m
-    | n < 0 || m < 0 = error "Please provide positive arguments for diffSelect!"
-    | otherwise = take n [ x | x <- randomRs (0, m) (mkStdGen m) ]
+    | n < 0 || m < 0    = error "Please provide positive arguments for diffSelect!"
+    | otherwise         = take n [ x | x <- randomRs (0, m) (mkStdGen m) ]
